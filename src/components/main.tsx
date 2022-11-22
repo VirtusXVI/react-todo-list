8176268de0149f86f9a8a7a6e33c9bfbba0e101a
VirtusXVI <manuel.aveta2001@gmail.com>
2022-11-22 19:05:56 +0100

    corrected variable add

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react'
 import '../assets/css/style.css'
 import Bin from '../assets/img/Bin.svg'
 import Checkbox from '../assets/img/Checkbox_Off.svg'
 
 function App() {
     let todoList = [{'name': 'Andare al Supermercato'},{'name': 'Andare a Casa'}];
-    let add: boolean = false;
+    // let add: boolean = false;
+    const [add, setAdd] = useState(false);
     return (
         <div className='app-surface'>
             <div className='todo-list'>
-                <div>
-                    <h1>TODO LIST</h1>
-                    <ul>
-                        { todoItems() }
-                    </ul>
-                </div>
+                {add ? 
+                    '' : 
+                    <div>
+                        <h1>TODO LIST</h1>
+                        <ul>
+                            { todoItems() }
+                        </ul>
+                    </div>
+                }
                 <div className='add-button'>
                     <button onClick={visibleAdd}>ADD</button>
                 </div>
+                {add ? <div>ciao</div> : ''
+
+                }
             </div>
             <div>
                 
@@ -34,7 +41,8 @@ function App() {
         return listItems;
     }
     function visibleAdd(){
-        add = !add;
+        setAdd(!add);
+        console.log(add);
     }
 }
 
