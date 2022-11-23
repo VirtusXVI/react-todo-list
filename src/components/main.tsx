9296c25cd9c617a99e2851d7063ba06e3c98a350
VirtusXVI <manuel.aveta2001@gmail.com>
2022-11-23 16:06:33 +0100

    completed delete button

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,48 +1,62 @@
 import React, { useState } from 'react'
 import '../assets/css/style.css'
 import Bin from '../assets/img/Bin.svg'
-import Checkbox from '../assets/img/Checkbox_Off.svg'
+import CheckboxOff from '../assets/img/Checkbox_Off.svg'
+import CheckboxOn from '../assets/img/Checkbox_On.svg'
 
 function App() {
-    let todoList = [{'name': 'Andare al Supermercato'},{'name': 'Andare a Casa'}];
-    // let add: boolean = false;
+    let [show, setShow] = useState(false)
+    let [newID, setNewID] = useState(1);
+    let [todoList,setTodoList] = useState([{'id': 1, 'name':'Andare al Supermercato'}]);
     const [add, setAdd] = useState(false);
+    const [newtodo, setNewtodo] = useState('');
     return (
         <div className='app-surface'>
             <div className='todo-list'>
-                {add ? 
+                {
+                add ? 
                     '' : 
                     <div>
                         <h1>TODO LIST</h1>
                         <ul>
                             { todoItems() }
                         </ul>
+                        <div className='add-button'>
+                            <button onClick={visibleAdd}>ADD</button>
+                        </div>
                     </div>
                 }
-                <div className='add-button'>
-                    <button onClick={visibleAdd}>ADD</button>
-                </div>
-                {add ? <div>ciao</div> : ''
-
+                {
+                add ? 
+                    <div>
+                        <input type="text" onChange={e => setNewtodo(e.target.value)}/>
+                        <div className='add-button'>
+                            <button onClick={addToArray}>ADD</button>
+                        </div>
+                    </div> 
+                : ''
                 }
-            </div>
-            <div>
-                
             </div>
         </div>
     );
     function todoItems(){
-        const listItems = todoList.map((d) => 
-        <li key={d.name}>
-            <img className='bin' src={ Bin } alt="Bin" />
-            <img className='checkbox' src={ Checkbox } alt="Checkbox" />
-            {d.name}
-        </li>);
-        return listItems;
+        return todoList.map(todo => (
+            <li key={todo.id}>
+                <img className='bin' onClick={() => setTodoList(todoList.filter(a => a.id !== todo.id))} src={ Bin } alt="Bin" />
+                <img className='checkbox' onClick={() => setShow(!show)} src={ CheckboxOff } alt="Checkbox" />
+                <span className={ show ? 'show' : '' }>{todo.name}</span>
+            </li>
+        ));
     }
     function visibleAdd(){
         setAdd(!add);
-        console.log(add);
+    }
+    function addToArray(){
+        setNewtodo('');
+        console.log(newID);
+        setTodoList([...todoList,{'id': newID + 1, 'name':newtodo}]);
+        setNewID(newID + 1);
+        setAdd(!add);
     }
 }
 
