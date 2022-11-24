e99dc3dd3bc0ab44010fc7fc82ed562f2c1211e2
VirtusXVI <manuel.aveta2001@gmail.com>
2022-11-24 11:01:11 +0100

    completed checkbox functionality

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -6,8 +6,8 @@ import CheckboxOn from '../assets/img/Checkbox_On.svg'
 
 function App() {
     let [show, setShow] = useState(false)
-    let [newID, setNewID] = useState(1);
-    let [todoList,setTodoList] = useState([{'id': 1, 'name':'Andare al Supermercato'}]);
+    let [newID, setNewID] = useState(0);
+    let [todoList,setTodoList] = useState([{'id': 0, 'name':'Andare al Supermercato', 'done': false}]);
     const [add, setAdd] = useState(false);
     const [newtodo, setNewtodo] = useState('');
     return (
@@ -40,11 +40,11 @@ function App() {
         </div>
     );
     function todoItems(){
-        return todoList.map(todo => (
+        return todoList.map((todo,index) => (
             <li key={todo.id}>
                 <img className='bin' onClick={() => setTodoList(todoList.filter(a => a.id !== todo.id))} src={ Bin } alt="Bin" />
-                <img className='checkbox' onClick={() => setShow(!show)} src={ CheckboxOff } alt="Checkbox" />
-                <span className={ show ? 'show' : '' }>{todo.name}</span>
+                <img src={ changeImage(index) } onClick={(e) => changeStatus(index)} alt="" />
+                <span className={`${todo.done ? 'show' : ''}`}>{todo.name}</span>
             </li>
         ));
     }
@@ -54,9 +54,21 @@ function App() {
     function addToArray(){
         setNewtodo('');
         console.log(newID);
-        setTodoList([...todoList,{'id': newID + 1, 'name':newtodo}]);
+        setTodoList([...todoList,{'id': newID + 1, 'name':newtodo, 'done':false}]);
         setNewID(newID + 1);
         setAdd(!add);
+    }
+    function changeStatus(id: number){
+        todoList[id].done = !todoList[id].done;
+        setTodoList([...todoList]);
+        console.log(todoList[id].done);
+    }
+    function changeImage(id: number){
+        if(todoList[id].done){
+            return CheckboxOn;
+        }else{
+            return CheckboxOff;
+        }
     }
 }
 
